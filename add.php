0145e1ef430e8b1d440912d3ede616d38d7f99fc
Oleksandr Natalenko <oleksandr@natalenko.name>
2013-11-21 13:34:51 +0200

    add.php: fix title

--- add.php
+++ add.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<link rel="stylesheet" href="styles.css" type="text/css">
-		<title>Medical Phonebook</title>
+		<title>Phonebook</title>
 	</head>
 	<body>
 		<form action="do_add.php" method="post">
